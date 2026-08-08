be3289765b4c53b09734c763287c23cb804190aa
dulbae <hafizhabdullah212@gmail.com>
2026-08-08 22:01:03 +0700

    first commit

---
+++ ApaAja.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+void Print()
+{
+    int a;
+
+    cout << "masukan nilai: " ;
+    cin >> a ; 
+
+    for (int i = 1; i < a; i++)
+    {
+        for (int j = 1; j < i; j++)
+        {
+            cout << "*";
+        }
+        
+        cout << endl;
+    }
+    
+
+}
+
+int main()
+{
+    Print();
+
+    return 0;
+}

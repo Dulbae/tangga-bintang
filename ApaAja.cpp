72fe6b7737b805851709de03d1a197449232d692
dulbae <hafizhabdullah212@gmail.com>
2026-08-09 21:37:07 +0700

    tangga terbalik

--- ApaAja.cpp
+++ ApaAja.cpp
@@ -9,9 +9,9 @@ void Print()
     cout << "masukan nilai: " ;
     cin >> a ; 
 
-    for (int i = 1; i < a; i++)
+    for (int i = a; i >= 1; i--)
     {
-        for (int j = 1; j < i; j++)
+        for (int j = 1; j <= i; j++)
         {
             cout << "*";
         }

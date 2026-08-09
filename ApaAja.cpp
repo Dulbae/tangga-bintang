bcc65011fe59b04980a56d58e2cf8d03cadfef2f
dulbae <hafizhabdullah212@gmail.com>
2026-08-09 22:00:25 +0700

    tangga bintang spasi

--- ApaAja.cpp
+++ ApaAja.cpp
@@ -11,6 +11,11 @@ void Print()
 
     for (int i = a; i >= 1; i--)
     {
+        for (int j = 1; j <= a - i; j++)
+        {
+            cout << " ";
+        }
+        
         for (int j = 1; j <= i; j++)
         {
             cout << "*";

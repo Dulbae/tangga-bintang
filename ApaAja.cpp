5a7e7f092515767ac711323231baaf7616df678a
dulbae <hafizhabdullah212@gmail.com>
2026-08-26 22:08:54 +0700

    recall2

--- ApaAja.cpp
+++ ApaAja.cpp
@@ -11,6 +11,10 @@ void Print()
 
     for (int i = 1; i <= a; i++)
     {
+        for (int j = 1; j <= a - i; j++)
+        {
+            cout << " ";
+        }
         
         for (int j = 1; j <= i; j++)
         {

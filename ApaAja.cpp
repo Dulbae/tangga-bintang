da4656dbaa70830c1a9f44cd6cf410d58b3d0f1e
dulbae <hafizhabdullah212@gmail.com>
2026-08-26 21:55:31 +0700

    recall

--- ApaAja.cpp
+++ ApaAja.cpp
@@ -9,12 +9,8 @@ void Print()
     cout << "masukan nilai: " ;
     cin >> a ; 
 
-    for (int i = a; i >= 1; i--)
+    for (int i = 1; i <= a; i++)
     {
-        for (int j = 1; j <= a - i; j++)
-        {
-            cout << " ";
-        }
         
         for (int j = 1; j <= i; j++)
         {

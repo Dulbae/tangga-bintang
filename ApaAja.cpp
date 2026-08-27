5bb1bedcdbbe28ced76f5c665dd9806bb9aef2db
dulbae <hafizhabdullah212@gmail.com>
2026-08-27 22:02:52 +0700

    tipis tipis

--- ApaAja.cpp
+++ ApaAja.cpp
@@ -9,14 +9,14 @@ void Print()
     cout << "masukan nilai: " ;
     cin >> a ; 
 
-    for (int i = 1; i <= a; i++)
+    for (int i = 1 ; i <= a; i++)
     {
-        for (int j = 1; j <= a - i; j++)
+        for (int j = 1; j <= i - 1; j++)
         {
             cout << " ";
         }
         
-        for (int j = 1; j <= i; j++)
+        for (int j = 1; j <= a - i + 1; j++)
         {
             cout << "*";
         }

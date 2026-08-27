d1b668e7b429687a92fcb1c4e7f8357242cf2825
dulbae <hafizhabdullah212@gmail.com>
2026-08-27 22:13:52 +0700

    tipis tipis lanjutan

--- ApaAja.cpp
+++ ApaAja.cpp
@@ -11,12 +11,12 @@ void Print()
 
     for (int i = 1 ; i <= a; i++)
     {
-        for (int j = 1; j <= i - 1; j++)
+        for (int j = 1; j <= a - i; j++)
         {
             cout << " ";
         }
         
-        for (int j = 1; j <= a - i + 1; j++)
+        for (int j = 1; j <= 2 * i - 1; j++)
         {
             cout << "*";
         }
